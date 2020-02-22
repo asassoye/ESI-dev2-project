d0eac6fe5f653cabe437043d98de681fa65563ea
Andrew SASSOYE <andrew@sassoye.be>
2020-02-22 20:02:46 +0100

    maven github setup

--- src/test/java/be/sassoye/andrew/dev2/project/MainAppTest.java
+++ src/test/java/be/sassoye/andrew/dev2/project/MainAppTest.java
@@ -1,13 +1,6 @@
 package be.sassoye.andrew.dev2.project;
 
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class MainAppTest {
-   @Test
-   public void testJUnit() {
-      assertEquals(1, 0);
-   }
+
 }
