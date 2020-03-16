0a6f27589a3aea88be32796283078a932d05a84b
Andrew Sassoye <andrew@sassoye.be>
2020-03-16 15:53:23 +0100

    model.Position

---
+++ src/main/java/g54327/humbug/model/Position.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2020 Andrew SASSOYE
+ *
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and associated documentation files (the “Software”),
+ * to deal in the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package g54327.humbug.model;
+
+import java.util.Objects;
+
+/**
+ * Position class. Contains row and column of the position.
+ *
+ * @author Andrew SASSOYE <andrew@sassoye.be>
+ */
+public class Position {
+    private final int row;
+
+    private final int column;
+
+    /**
+     * Constructor
+     *
+     * @param row
+     * @param column
+     */
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    /**
+     * Equals method
+     *
+     * @param o Object to compare with.
+     * @return true if they are equal, false if not.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return row == position.row &&
+                column == position.column;
+    }
+
+    /**
+     * HashCode method
+     *
+     * @return hash code.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+
+    /**
+     * Row getter
+     *
+     * @return row
+     */
+    public int getRow() {
+        return row;
+    }
+
+    /**
+     * Column getter
+     *
+     * @return column
+     */
+    public int getColumn() {
+        return column;
+    }
+}
